63ae0d17047fa7d54adf76e83745e97df7e394ec
adiz <weszerle42@gmail.com>
2025-02-28 13:06:50 +0100

    Újabb feladatok, kommentelés, megjegyzések.

--- test_html.py
+++ test_html.py
@@ -27,10 +27,24 @@ def test_file_structure():
 html_soup = get_html() #bs4 html oldal
 css_list = get_css() #cssutils lista
 
+#Megjegyzések:
+#Ha CSS-t kell ellenőrizni, a GetPropertyValue függvényt csináltam hozzá.
+#Egyszerű a használata: Az első paramétere a css selector, a második a tulajdonság,
+#és ha mind a kettő létezik, akkor visszadobja az értéket, amit lehet assert-el ellenőrizni.
+
+#Ha HTML-t kell ellenőrizni, akkor a html_soup.find-al lehet
+#elementet keresni, első paramétere az element típusa.
+#A második paramétere lehet class, id, stb.
+#A class-okat (és gondolom az id-kat is) listaként dobja vissza,
+#szóval az első elemre kell ellenőrizni, ha csak egy class van megadva.
+#A .name tulajdonság az element típusát adja vissza.
+#Az elementek pozícióját személy szerint én az előző/következő elementekkel ellenőrzöm,
+#és ha kell akkor a parent/children-el is.
+
 def GetPropertyValue(selector: str, propName: str):
     assert any(x for x in css_list if x.selectorText == selector), f"Nincs {selector} osztály!" #Osztály létezés ellenőrzés
-    assert next(x for x in css_list if x.selectorText == selector).style.getPropertyValue(propName), f"Nincs {propName} tulajdonság!" 
-    return next(x for x in css_list if x.selectorText == selector).style.getPropertyValue(propName)
+    assert next(x for x in css_list if x.selectorText == selector).style.getPropertyValue(propName), f"Nincs {propName} tulajdonság!" #Tulajdonság ellenőrzés az osztályban
+    return next(x for x in css_list if x.selectorText == selector).style.getPropertyValue(propName) #Tulajdonság értékének visszadobása
 
 #Feladatok
 @pytest.mark.points(1)
@@ -68,3 +82,38 @@ def test_feladat_6():
     for tag, text in elems:
         assert html_soup.find(class_="hetek").find(name=tag) != None, f"Nem létezik {tag} típusú elem a .hetek div-ben!"
         assert html_soup.find(class_="hetek").find(name="span") != None, f"Nem létezik olyan elem a .hetek div-ben, aminek a szövege '{text}' lenne!"
+
+@pytest.mark.points(1)
+def test_feladat_7():
+    assert GetPropertyValue(".hetek", "display") == "flex", "Nincs flex rendezés a .hetek kiválasztón!"
+    assert GetPropertyValue(".hetek", "justify-content") == "space-evenly", "Helytelen a rendezés módja!"
+
+@pytest.mark.points(1)
+def test_feladat_8():
+    assert GetPropertyValue("hr", "background-color") == "red", "Helytelen/hiányzik a háttérszín beállítás!"
+    assert GetPropertyValue("hr", "border-color") == "red", "Helytelen/hiányzik a szegélyszín beállítás!"
+
+@pytest.mark.points(1)
+def test_feladat_9():
+    prev_sibl = None #Előző element
+    target = None #Keresett element
+    next_sibl = None #Következő element
+
+    target = html_soup.find("hr")
+    prev_sibl = target.find_previous_sibling()
+    next_sibl = target.find_next_sibling()
+
+    assert isinstance(target, bs4.Tag), "Nem található hr elem az oldalon!"
+    assert prev_sibl.get("class")[0] == "hetek", "Helytelen a hr elem elhelyezése!"
+    assert next_sibl.name == "hr", "Hiányzik a második hr elem!"
+
+@pytest.mark.points(2)
+def test_feladat_10():
+    target = None
+
+    target = html_soup.find(class_="egynap")
+
+    assert isinstance(target, bs4.Tag), "Nincs egynap osztályú elem!"
+    assert target.name == "div", "Az egynap osztályú elem nem div típusú!"
+    assert target.find_previous_siblings()[0].name == "hr", "Helytelen az egynap elem elhelyezése!"
+    assert target.find_previous_siblings()[1].name == "hr", "Helytelen az egynap elem elhelyezése!"
